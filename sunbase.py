22c68a924abf9d854bde2e1cc2d99e2b3b2babd3
Anubhav Dhull <124768416+databhav@users.noreply.github.com>
2023-10-19 18:27:21 +0530

    Update sunbase.py

--- sunbase.py
+++ sunbase.py
@@ -3,8 +3,6 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split
